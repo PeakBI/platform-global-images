f2e66e381fd2520f722e4737ae08d962dd3d8b77
chrisbillingham_peakai <chris.billingham@peak.ai>
2022-11-01 14:26:30 +0000

    manual

---
+++ workspace/r/ds-pack/v2/install_r_packages.sh
@@ -0,0 +1,28 @@
+install2.r --error --deps TRUE -l /usr/local/lib/R/library \
+  broom \
+  dbplyr \
+  dplyr \
+  dtplyr \
+  forcats \
+  ggplot2 \
+  googledrive \
+  googlesheets4 \
+  haven \
+  hms \
+  httr \
+  jsonlite \
+  lubridate \
+  magrittr \
+  modelr \
+  pillar \
+  purrr \
+  readr \
+  readxl \
+  reprex \
+  rlang \
+  rvest \
+  stringr \
+  tibble \
+  tidyr \
+  xml2 \
+  tidyverse 
